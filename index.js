4ab5251fac7c2348cf05815635fe1d0a684fe489
mjvini <viniciustorres4@gmail.com>
2023-10-12 20:48:09 -0300

    navbar quase lá

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleNavbar() {
+    var navLinks = document.getElementById("navLinks");
+    if (navLinks.style.display === "block") {
+      navLinks.style.display = "none";
+    } else {
+      navLinks.style.display = "block";
+    }
+  }
